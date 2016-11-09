abc0a59317b72fc974b929664324c0333d467608
Muhammad Bilal Kadri <bilal.kadri@gmail.com>
2016-11-09 18:37:59 +0500

    Extending code for multiple obstacles

--- Matlab Files for Jounral Pape/Bringing_all_on_the same_path/CC_OA_Creating_Formation_Dyn_Model_Mul_Obs.m
+++ Matlab Files for Jounral Pape/Bringing_all_on_the same_path/CC_OA_Creating_Formation_Dyn_Model_Mul_Obs.m
@@ -11,6 +11,7 @@ close all;
 %Initialization of parameters
 %-----------------------------------------------------------------------
 number_of_robots=5;
+number_of_obstacles=2;
 
 %Dynamic characteristic of the robot
 %My values with which code is working
@@ -95,7 +96,7 @@ robot_Trajectory_Vec=[];
 virtual_robot_Trajectory_Vec=[];
 
 %-----------------------------------------------------------------------
-%                   Code for Animated LInes
+%                   Code for Animated Lines
 %------------------------------------------------------------------------
 
 color = 'kbgrcmy'; colorVal=1;
@@ -120,70 +121,83 @@ Virtual_robotTrajectory_ZP = [animatedline('Color',color(7),'LineWidth',2)];
 %the ellipse envelops a rectangle with (x0+-v1,y0+-v2) as its vertices
 
 %defining some rectangular obstacles
-origin_of_rectangle=[20,20];
-width=4;
-height=3;
+% First Obstacle
+origin_of_rectangle(1,:)=[20,20];
+width(1)=4;
+height(1)=3;
+
+% Second Obstacle
+origin_of_rectangle(2,:)=[40,30];
+width(2)=4;
+height(2)=3;
 
 figure(1);
-rectangle('Position',[origin_of_rectangle width height],'Curvature',[0.05 0.05]),...
+rectangle('Position',[origin_of_rectangle(1,:) width(1) height(1)],'Curvature',[0.05 0.05]),...
+    axis equal,hold on
+rectangle('Position',[origin_of_rectangle(2,:) width(2) height(2)],'Curvature',[0.05 0.05]),...
     axis equal,hold on
 
-% axis([-10 20 -10 20])
 
-xa=origin_of_rectangle(1);
-ya=origin_of_rectangle(2);
+x=0:0.1:50;
 
-v1=width/2;
-v2=height/2;
-%Center of the obstacle
-x0=xa+width/2;
-y0=ya+height/2;
-
-%Plotting the ellipse encircling the obstacle
-%defining range for x
-x=-20:0.1:40;
-
-A=sqrt(1/(2*(v1^2)));
-B=sqrt(1/(2*(v2^2)));
-
-y1=real(sqrt((1-A^2*(x-x0).^2)/B^2)+y0);
-y2=real(-sqrt((1-A^2*(x-x0).^2)/B^2)+y0);
-plot(x,y1,'g',x,y2,'g'),axis tight,grid on,hold on
-
-% B1=0.1:0.1:0.5
-% A1=(A/B)*B1
-
-%Defining ellipses around the obstacles
-v1_expansion=v1:v1/10:v1*1.25;
-A1=sqrt(1./(2*(v1_expansion.^2)));
-B1=(B/A)*A1;
-
-for i=1:length(B1)
-    %     if(B1(i))>B
-    %         disp('I am here')
-    %     else
-    y1=real(sqrt((1-A1(i)^2*(x-x0).^2)/B1(i)^2)+y0);
-    y2=real(-sqrt((1-A1(i)^2*(x-x0).^2)/B1(i)^2)+y0);
-    plot(x,y1,'--k',x,y2,'--k'),axis tight,grid on,hold on
-    %     end
+for i=1:number_of_obstacles
+    %Defining ellipses around the obstacles
+    
+    xa(i)=origin_of_rectangle(i,1);
+    ya(i)=origin_of_rectangle(i,2);
+    
+    v1(i)=width(i)/2;
+    v2(i)=height(i)/2;
+    
+    
+    %Center of the obstacle
+    x0(i)=xa(i)+width(i)/2;
+    y0(i)=ya(i)+height(i)/2;
+    
+    %Plotting the ellipse encircling the obstacle
+    %defining range for x
+    A(i)=sqrt(1/(2*(v1(i)^2)));
+    B(i)=sqrt(1/(2*(v2(i)^2)));
+    
+    y1(i,:)=real(sqrt((1-A(i)^2*(x-x0(i)).^2)/B(i)^2)+y0(i));
+    y2(i,:)=real(-sqrt((1-A(i)^2*(x-x0(i)).^2)/B(i)^2)+y0(i));
+    
+    plot(x,y1(i,:),'g',x,y2(i,:),'g'),axis tight,grid on,hold on
+    
+    v1_expansion(i,:)=v1(i):v1(i)/10:v1(i)*1.25;
+    
+    A1(i,:)=sqrt(1./(2*(v1_expansion(i,:).^2)));
+    B1(i,:)=(B(i)/A(i))*A1(i,:);
+    
+    for j=1:length(B1(1,:))
+        
+        y1(i,:)=real(sqrt((1-A1(i,j)^2*(x-x0(i)).^2)/B1(i,j)^2)+y0(i));
+        y2(i,:)=real(-sqrt((1-A1(i,j)^2*(x-x0(i)).^2)/B1(i,j)^2)+y0(i));
+        plot(x,y1(i,:),'--k',x,y2(i,:),'--k'),axis tight,grid on,hold on
+        %     end
+    end
     
 end
-%------------------------------------------------------------------------
+
+
+------------------------------------------------------------------------
+
+
 
 
 for t=1:700  %total simulation time [The main BIG Loop]
     %     robot{i}=[x_pos_new y_pos_new];
     robot_Trajectory_Vec=[robot_Trajectory_Vec;robot];
     t
-    
+
     for i=1:number_of_robots
         dummy=robot(i);
         xy_pos=cell2mat(dummy);
         x_pos(i)=xy_pos(1);
         y_pos(i)=xy_pos(2);
     end
-    
-    
+
+
     %----------------------------------------------------------------------
     %Calculation of the angle with which robot is approaching the obstacle
     %We are using condition t>3 because the robot must have moved a little
@@ -191,7 +205,7 @@ for t=1:700  %total simulation time [The main BIG Loop]
     %based on its current and previous position.
     if(t>3)
         for i=1:number_of_robots
-            
+
             final_pos=cell2mat(robot_Trajectory_Vec(t-1,i));
             initial_pos=cell2mat(robot_Trajectory_Vec(t-2,i));
             %         robot_Trajectory_Vec{t-2}
@@ -208,22 +222,18 @@ for t=1:700  %total simulation time [The main BIG Loop]
     %If the 'test' value is approx 1e-3 it means that the robot uis very
     %near the ellipse. If the robot is exactly on the ellipse then 'test'
     %must be ZERO. [This was the first approach I was using for obstacle avoidance]
-    
+
     %The second approach for testing whetner the robot is approaching the
     %obstacle or not is based on the journal paper, it compares ra with rk
     %and then takes a decision.
-    
+
     for i=1:number_of_robots
-        %     test(i)=A1(end)^2*(x_pos(i)-x0)^2+B1(end)^2*(y_pos(i)-y0)^2-1;
-        
+      
         rk(i)=sqrt(A^2*(x_pos(i)-x0)^2+B^2*(y_pos(i)-y0)^2-1);
-        
-        
-        
         %---------------------------------------------------------------------
         %   Defining forces to avoid obstacle
         %---------------------------------------------------------------------
-  
+
         if (rk(i)<=ra(i))
             %         fxkOA=fxkdes;
             %         fykOA=fykdes;
@@ -233,9 +243,9 @@ for t=1:700  %total simulation time [The main BIG Loop]
             fykrc(i)=-(A/B)*(x_pos(i)-x0);
             fxkrcc(i)=-(B/A)*(y_pos(i)-y0);
             fykrcc(i)=(A/B)*(x_pos(i)-x0);
-            
+
             %             fkdes(i)=sqrt(fxkdes(i)^2+fykdes(i)^2);
-            
+
             if (mod(Psi(i)-Chi(i),2*pi)<=pi)   %Psi>=Chi
                 fxkr(i)=fxkrc(i);
                 fykr(i)=fykrc(i);
@@ -243,57 +253,57 @@ for t=1:700  %total simulation time [The main BIG Loop]
                 fxkr(i)=fxkrcc(i);
                 fykr(i)=fykrcc(i);
             end
-            
-            
+
+
             mod_fkr(i)=sqrt(fxkr(i)^2+fykr(i)^2);
             fxkrn(i)=fxkr(i)/mod_fkr(i);
             fykrn(i)=fykr(i)/mod_fkr(i);
-            
+
         end
     end
-    
+
     %----------------------------------------------------------------------
     %----------------------------------------------------------------------
     %            Computing the new position of Virtual Robot
     %----------------------------------------------------------------------
     %----------------------------------------------------------------------
-    
+
     %Code for updating the Virtual robot position when I am using Obstacles
     rm=min(rk);
-    
-    
+
+
     %     if (sum(OBSTACLE)==0)
     if (rm>ra)
         fxvBS_virtual=fxvdes;
         fyvBS_virtual=fyvdes;
-        
+
     elseif (rm<=ra)
         xm=mean(x_pos);
         ym=mean(y_pos);
-        
+
         fxvBS_virtual=fxvdes+kp*(xm-hyp_robot(1))*(1-exp(-tau*rm));
         fyvBS_virtual=fyvdes+kp*(ym-hyp_robot(2))*(1-exp(-tau*rm));
     end
-    
-    
+
+
     fx_virtual = @(t,x) [x(2); (fxvBS_virtual-(B_model+kd)*x(2))/M];
     [T,X_virtual]=ode45(fx_virtual,tspan_ode45,[hyp_robot(1);xdot_virtual(i)]);
     [m,z] = size(X_virtual);
     x_pos_new_virtual=real(X_virtual(m,1));
     xdot_virtual(i)=real(X_virtual(m,2));
-    
-    
+
+
     fy_virtual = @(t,y) [y(2); (fyvBS_virtual-(B_model+kd)*y(2))/M];
     [T,Y_virtual]=ode45(fy_virtual,tspan_ode45,[hyp_robot(2);ydot_virtual(i)]);
     [m,z] = size(Y_virtual);
     y_pos_new_virtual=real(Y_virtual(m,1));
     ydot_virtual(i)=real(Y_virtual(m,2));
-    
-    
+
+
     hyp_robot=[x_pos_new_virtual y_pos_new_virtual];
     virtual_robot_Trajectory_Vec=[virtual_robot_Trajectory_Vec;hyp_robot];
-    
-    
+
+
     addpoints(Virtual_robotTrajectory_ZP,x_pos_new_virtual,y_pos_new_virtual);
     drawnow
     %---------------------------------------------------------------------
@@ -301,12 +311,12 @@ for t=1:700  %total simulation time [The main BIG Loop]
     %            End of code Computing the new position of Virtual Robot
     %----------------------------------------------------------------------
     %----------------------------------------------------------------------
-    
+
     %Computing the force for the first robot
     %Equation (6) of the paper
-    
+
     %Computing distance of individual robot from all other robots
-    
+
     for i=1:number_of_robots
         for j=1:number_of_robots
             a=robot{i}-robot{j};
@@ -323,9 +333,9 @@ for t=1:700  %total simulation time [The main BIG Loop]
             EF_y(i,j)=Electrostatic_Forces(i,j)*sin(theta(i,j));
         end
     end
-    
-    
-    
+
+
+
     for i=1:number_of_robots
         a=robot{i}-hyp_robot;
         Attractive_Force_x(i)=ksk*(a(1)*(a(1)^2+a(2)^2-alpha^2));
@@ -333,29 +343,29 @@ for t=1:700  %total simulation time [The main BIG Loop]
         fxkVS(i)=kr*sum(EF_x(i,1:number_of_robots))-Attractive_Force_x(i);
         fykVS(i)=kr*sum(EF_y(i,1:number_of_robots))-Attractive_Force_y(i);
         fkVS(i)=sqrt(fxkVS(i)^2+fykVS(i)^2);
-        
+
         dummy=robot(i);
         xy_pos=cell2mat(dummy);
         x_pos(i)=xy_pos(1);
         y_pos(i)=xy_pos(2);
-        
-        
+
+
         %initializing x_pos_new and y_pos_new
         x_pos_new=x_pos(i);
         y_pos_new=y_pos(i);
-        
+
         distance_from_hyp_robot=sqrt((x_pos(i)-hyp_robot(1))^2+(y_pos(i)-hyp_robot(2))^2);
-        
+
         %---------------------------------------------------------------
         %    Forces with obstacles (Behavioral structure)
         %---------------------------------------------------------------
-        
+
         %Force when the robot is approaching the obstacle
-        
+
         if ((rk(i)-ra(i))<1e-3)
             %             disp('force when rk=ra');
-            
-            
+
+
             if (FLAG(i)==0)
                 fxkVS_rk_ra(i)=fxkVS(i);
                 fykVS_rk_ra(i)=fykVS(i);
@@ -363,26 +373,26 @@ for t=1:700  %total simulation time [The main BIG Loop]
                 FLAG(i)=FLAG(i)+1;
             end
         end
-        
+
         if(rk(i)<ra(i))
-            
+
             FxkBS(i)= fxkVS_rk_ra(i)+((fkVS_rk_ra(i)*fxkrn(i))/(rk(i)^2))*(1/rk(i)-1/ra(i));
             FykBS(i)= fykVS_rk_ra(i)+((fkVS_rk_ra(i)*fykrn(i))/(rk(i)^2))*(1/rk(i)-1/ra(i));
-            
+
             %Force when the robot has passed the obstacle
         elseif (rk(i)>ra(i))
-     
+
             FxkBS(i)= fxkVS_rk_ra(i)*exp(-tau*rk(i))+fxkVS(i)*(1-exp(-tau*rk(i)));
             FykBS(i)= fykVS_rk_ra(i)*exp(-tau*rk(i))+fykVS(i)*(1-exp(-tau*rk(i)));
-                     
-            
+
+
         end
         fx = @(t,x) [x(2); (FxkBS(i)-(B_model+kd)*x(2))/M];
         [T,X]=ode45(fx,tspan_ode45,[x_pos(i);xdot(i)]);
         [m,z] = size(X);
         x_pos_new=real(X(m,1));
         xdot(i)=real(X(m,2));
-        
+
         fy = @(t,y) [y(2); (FykBS(i)-(B_model+kd)*y(2))/M];
         [T,Y]=ode45(fy,tspan_ode45,[y_pos(i);ydot(i)]);
         [m,z] = size(Y);
@@ -392,14 +402,14 @@ for t=1:700  %total simulation time [The main BIG Loop]
         addpoints(robotTrajectory_ZP(i),x_pos_new,y_pos_new);
         %         addpoints(Virtual_robotTrajectory_ZP,x_pos_new,y_pos_new);
         drawnow
-        
+
     end
-    
-    
+
+
     robots_trajectory=cell2mat(robot_Trajectory_Vec);
     k=1;
     for i=1:number_of_robots
-        
+
         X_for_polygon(i)= robots_trajectory(end,k);
         Y_for_polygon(i)=robots_trajectory(end,k+1);
         k=k+2;
@@ -411,8 +421,8 @@ for t=1:700  %total simulation time [The main BIG Loop]
     end
     h1=plot(X_for_polygon(k),Y_for_polygon(k));
     robots_trajectory=cell2mat(robot_Trajectory_Vec);
-    
-    
+
+
 end %end of the bigger loop
 
 % figure(1);
